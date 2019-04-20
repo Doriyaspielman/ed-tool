e4783af6f605ea88161c150cf298e6e86f797949
noytvili <33618257+noytvili@users.noreply.github.com>
2019-04-20 17:48:42 +0300

    Update Editor.h

--- Editor.h
+++ Editor.h
@@ -1,2 +1,14 @@
 /* Doriya Spielman 313466625 */
 /* Noy Tvili 308426790 */
+
+#include <vector>
+using namespace std;
+
+class Editor{
+private:
+    int num_of_line; // for this action: "7 makes line #7 the current line"
+    vector <string> current_Line; //vector of lines (each line = string)
+
+public:
+    void loop();    
+}
