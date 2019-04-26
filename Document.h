e67e6a863950a1e0dae8096ee6687c8eaf34351d
noytvili <33618257+noytvili@users.noreply.github.com>
2019-04-26 12:59:42 +0300

    Update Document.h

--- Document.h
+++ Document.h
@@ -1,2 +1,29 @@
 /* Doriya Spielman 313466625 */
 /* Noy Tvili 308426790 */
+
+#include <vector>
+#include <string>
+using namespace std;
+
+class Document {
+    private:
+        vector<string> document_String ;
+        int current_Line;
+        
+    public:
+        Document();
+        int getCurrent_Line() const;
+        void setCurrent_Line(const int&);
+        string getString_Line();
+        void deleteCurrent_Line();
+        vector<string> getDocument();
+        void insertNewLine_Before(const string& std);
+        void insertNewLine_After(const string& std);
+        int getNum_Of_Line();
+        void changeCurrent_Line(const string& newString);
+
+        /*
+        void insertLineByIndex(const int&,const std::string&);
+        bool setNewString(const std::string&,const std::string&);
+        */
+};
