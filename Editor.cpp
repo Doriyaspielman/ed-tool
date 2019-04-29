1d61a06fc14e78efc0571df0d6bd4799edd3851a
noytvili <33618257+noytvili@users.noreply.github.com>
2019-04-29 12:04:41 +0300

    Update Editor.cpp

--- Editor.cpp
+++ Editor.cpp
@@ -9,53 +9,63 @@ using namespace std;
 
 Editor::Editor() {
     cout << "Editor constructor- ckeck" << endl;  //after chech- delete
-    document;
+    this->document = {};
     this->userInput = "";
     loop();
 }
 
 void Editor::loop(){
     string command;
-    while (this->)
+    bool flag = true;
+   
+    while (flag){
+        cin >> command;
+        if (command == "Q"){
+           // Quits the editor without saving 
+           flag = false;
+           break;
+        }
 
-    cin >> command;
+        else if (command == "P"){
+            // prints the current line (ed maintains a current line) 
+            cout << this->document.getString_Line() << endl;    
+        }
 
+        else if (command == "n"){
+            //prints line number of current line followed by TAB followed by current line 
+            cout << this->document.getCurrent_Line() << "\t" << this->document.getString_Line << endl;
+        }
+        else if (command == "%p"){
+           // prints all lines
+           for(int i = 0; i < this->document.getNum_Of_Line(); i++){
+               cout << this->document.getDocument().at(i) << endl;
+           }
+        }
+        else if (command == מספר){
+            //makes line #מספר the current line 
+            //צריך לחשוב איך להתייחס לכל המספרים האפשריים
+        }
+        else if (command == "a"){
+            appends new text after the current line  
+        }
+        else if (command == "i"){
+            inserts new text before the current line  
+        }
+        else if (command == "c"){
+            changes the current line for text that follows 
+        }
+        else if (command == "d"){
+            //deletes the current line 
+            this->document.deleteCurrent_Line();
+        }
+        else if (command == "s/old/new/"){
+            replaces old string with new in current line (google: C++ split or token) 
+        }
 
-    if (command == "P"){
-        prints the current line (ed maintains a current line) 
-    }
-
-    if (command == "n"){
-        prints line number of current line followed by TAB followed by current line 
-    }
-    if (command == "%p"){
-        prints all lines 
-    }
-    if (command == מספר){
-        makes line #מספר the current line 
-        צריך לחשוב איך להתייחס לכל המספרים האפשריים
-    }
-    if (command == "a"){
-        appends new text after the current line  
-    }
-    if (command == "i"){
-      inserts new text before the current line  
-    }
-    if (command == "c"){
-        changes the current line for text that follows 
-    }
-    if (command == "d"){
-        deletes the current line 
-    }
-    if (command == "s/old/new/"){
-        replaces old string with new in current line (google: C++ split or token) 
-    }
-    if (command == "Q"){
-        Quits the editor without saving 
-    }
-    else{
-        /text searches forward after current line for the specified text.
-        The search wraps to the beginning of the buffer and continues down to the current line, if necessary 
-    }
+        else{
+            /text searches forward after current line for the specified text.
+            The search wraps to the beginning of the buffer and continues down to the current line, if necessary 
+        }
       
+    }
 }
